9aa3a054316fcbac74c8c8b89f4592ca51267726
zeebo <leterip@gmail.com>
2010-11-09 13:13:27 -0500

    removed useless file

--- sheet_parser.py
+++
@@ -1,8 +0,0 @@
-
-#The idea of this is to parse an XLS file for structure to extract the headers/titles/data.
-#Hopefully.
-#Gotta define how it fails when everything is just screwed up.
-
-#I think this has to be a gui.
-#a table editor basically that you copy/paste into, and make it all line up. No problem.
-#shouldn't be too hard. should try to make it one day.
